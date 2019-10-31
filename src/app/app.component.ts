819107203a80c14283d9bc425bace47a5f9ea638
Linh Vu <thuylinh2490@gmail.com>
2019-10-31 10:47:35 +0700

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'anglular-form-login';
+  title = 'Angular-form-login';
 }
